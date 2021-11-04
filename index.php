cdd901400d9ff088ad09b261f953d187a529a62a
Ardiantoeko <ardiantotechno@gmail.com>
2021-11-04 13:32:43 +0700

    test

--- index.php
+++ index.php
@@ -1,51 +1,142 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-     <!-- Template CSS -->
-      <link rel="stylesheet" href="stisla-master/assets/css/components.css">
-      <link rel="stylesheet" href="stisla-master/assets/css/style.css">
-      <link rel="stylesheet" type="text/css" href="assets/style.css">
-    <title>Gue Punya</title>
-  </head>
-  <body>
-    <!-- navbar -->
-    <nav class="navbar navbar-light bg-light fixed-top">
-        <a class="navbar-brand" href="#">
-        <h4 class="mb-0 text-danger">Gue Punya</h4>
-        <h6 class="text-center text-danger">Blogs</h6>
-        </a>
-    </nav>
-    <!-- akhir navbar -->
-    
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="stisla-master/assets/css/components.css">
+  <link rel="stylesheet" href="stisla-master/assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <title>Gue Punya</title>
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class=" my-navbar">
+    <div class="navbar-head">
+      <h4 class="mb-0 ml-4 text-danger">Blogs</h4>
+      <h6 class=" ml-4 text-danger">Gue Punya</h6>
+    </div>
+  </nav>
+  <!-- akhir navbar -->
+
+  <section id="section">
+    <div class="row">
+      <div class="col-md">
+        <div class="header">
+          <h5 class="text-center">
+            blogs
+          </h5>
+        </div>
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="row">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+          <article class="article article-style-b">
+            <div class="article-header">
+              <div class="article-image" data-background="../assets/img/news/img13.jpg">
+              </div>
+              <div class="article-badge">
+                <div class="article-badge-item bg-danger"><i class="fas fa-fire"></i> Trending</div>
+              </div>
+            </div>
+            <div class="article-details">
+              <div class="article-title">
+                <h2><a href="#">Excepteur sint occaecat cupidatat non proident</a></h2>
+              </div>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. </p>
+              <div class="article-cta">
+                <a href="#">Read More <i class="fas fa-chevron-right"></i></a>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+          <article class="article article-style-b">
+            <div class="article-header">
+              <div class="article-image" data-background="../assets/img/news/img15.jpg">
+              </div>
+            </div>
+            <div class="article-details">
+              <div class="article-title">
+                <h2><a href="#">Excepteur sint occaecat cupidatat non proident</a></h2>
+              </div>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. </p>
+              <div class="article-cta">
+                <a href="#">Read More <i class="fas fa-chevron-right"></i></a>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+          <article class="article article-style-b">
+            <div class="article-header">
+              <div class="article-image" data-background="../assets/img/news/img07.jpg">
+              </div>
+            </div>
+            <div class="article-details">
+              <div class="article-title">
+                <h2><a href="#">Excepteur sint occaecat cupidatat non proident</a></h2>
+              </div>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. </p>
+              <div class="article-cta">
+                <a href="#">Read More <i class="fas fa-chevron-right"></i></a>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3">
+          <article class="article article-style-b">
+            <div class="article-header">
+              <div class="article-image" data-background="../assets/img/news/img02.jpg">
+              </div>
+            </div>
+            <div class="article-details">
+              <div class="article-title">
+                <h2><a href="#">Excepteur sint occaecat cupidatat non proident</a></h2>
+              </div>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. </p>
+              <div class="article-cta">
+                <a href="#">Read More <i class="fas fa-chevron-right"></i></a>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
 
 
+  <!-- Optional JavaScript; choose one of the two! -->
 
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
 
+  <!-- General JS Scripts -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    
-    <!-- General JS Scripts -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-     <script src="stisla-master/assets/js/scripts.js"></script>
-     <script src="stisla-master/assets/js/stisla.js"></script>
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+  <script src="stisla-master/assets/js/scripts.js"></script>
+  <script src="stisla-master/assets/js/stisla.js"></script>
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
